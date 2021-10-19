5b8546f62965c8f5b66b0cbf2d19ff747e23770f
vhstavares <41763619+vhstavares@users.noreply.github.com>
2021-10-19 14:10:11 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
+import os
+import time
+
+app = Flask(__name__)
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo="Analisador de Mudas por meio de CNN" ,imagem = "capa_padrao.jpg")
+
+
+@app.route('/analize', methods=['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        print(request.files['arquivo'])
+        arquivo = request.files['arquivo']
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        arquivo.save(f'{upload_path}/seed-{timestamp}.jpg')
+        print(f'{upload_path}/seed-{timestamp}.jpg')
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
